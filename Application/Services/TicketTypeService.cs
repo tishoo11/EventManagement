49988b6e01a0274eaf5452bd057d99b8370b9005
Kebabche11 <tishoyanev08@gmail.com>
2026-06-10 22:30:24 +0300

    created service for TicketType

--- Application/Services/TicketTypeService.cs
+++ Application/Services/TicketTypeService.cs
@@ -9,7 +9,25 @@ namespace EventManagement11.Application.Services;
 
 public class TicketTypeService
 {
-    private readonly ITicketTypeRepository _ticketTypes;
-    private readonly ITicketRepository _tickets;
+    private readonly ITicketTypeRepository ticketTypes;
 
+    public TicketTypeService(ITicketTypeRepository ticketTypes)
+    {
+        this.ticketTypes = ticketTypes;
+    }
+
+    public void Create(TicketType entity) => ticketTypes.Save(entity);
+
+    public void Edit(TicketType entity) => ticketTypes.Update(entity);
+
+    public void Delete(int id)
+    {
+        var entity = ticketTypes.GetById(id);
+        if (entity != null)
+        {
+            ticketTypes.Delete(entity);
+        }
+    }
+
+    public IReadOnlyList<TicketType> GetAll() => ticketTypes.GetAll();
 }
