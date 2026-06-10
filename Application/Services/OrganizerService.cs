2654c155caa484d1da9753e97377f9e99525d5d3
Kebabche11 <tishoyanev08@gmail.com>
2026-06-10 22:29:57 +0300

    created services for Organizer and Ticket

--- Application/Services/OrganizerService.cs
+++ Application/Services/OrganizerService.cs
@@ -4,8 +4,25 @@ namespace EventManagement11.Application.Services;
 
 public class OrganizerService
 {
-    private readonly IOrganizerRepository _organizers;
-    private readonly IEventRepository _events;
+    private readonly IOrganizerRepository organizers;
 
-    
+    public OrganizerService(IOrganizerRepository organizers)
+    {
+        this.organizers = organizers;
+    }
+
+    public void Create(Organizer entity) => organizers.Save(entity);
+
+    public void Edit(Organizer entity) => organizers.Update(entity);
+
+    public void Delete(int id)
+    {
+        var entity = organizers.GetById(id);
+        if (entity != null)
+        {
+            organizers.Delete(entity);
+        }
+    }
+
+    public IReadOnlyList<Organizer> GetAll() => organizers.GetAll();
 }
--- Application/Services/TicketService.cs
+++ Application/Services/TicketService.cs
@@ -1,9 +1,52 @@
 
+using EventManagement11.Application.Interfaces;
 using EventManagement11.Domain.Entities;
 
 namespace EventManagement11.Application.Services;
 
 public class TicketService
 {
+    private readonly ITicketRepository tickets;
 
+    public TicketService(ITicketRepository tickets)
+    {
+        this.tickets = tickets;
+    }
+
+    public string GenerateCode() => Guid.NewGuid().ToString("N");
+
+    public Ticket Create(int eventId, TicketType ticketType)
+    {
+        var ticket = new Ticket(eventId, ticketType, GenerateCode());
+        tickets.Save(ticket);
+        return ticket;
+    }
+
+    public void Cancel(int id)
+    {
+        var ticket = tickets.GetById(id);
+        if (ticket != null)
+        {
+            ticket.Cancel();
+            tickets.Update(ticket);
+        }
+    }
+
+    public void MarkAsUsed(int id)
+    {
+        var ticket = tickets.GetById(id);
+        if (ticket != null)
+        {
+            ticket.MarkAsUsed();
+            tickets.Update(ticket);
+        }
+    }
+
+    public bool IsValid(int id)
+    {
+        var ticket = tickets.GetById(id);
+        return ticket?.IsValid == true;
+    }
+
+    public IReadOnlyList<Ticket> GetAll() => tickets.GetAll();
 }
