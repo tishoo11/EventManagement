5d4a107fc62955804cffac095d1021c54fff5321
Kebabche11 <tishoyanev08@gmail.com>
2026-06-20 11:52:42 +0300

    UI 43 - Program.cs

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,17 +1,34 @@
-﻿using EventManagement11.ConsoleUI;
-using Microsoft.EntityFrameworkCore;
+﻿using EventManagement11.Application.Services;
+using EventManagement11.Infrastructure;
+using EventManagement11.Infrastructure.Repositories;
 
-namespace PersonalWallet.ConsoleUI
+namespace EventManagement11.ConsoleUI;
+
+internal static class Program
 {
-    internal class Program
+    private static void Main()
     {
-        static void Main(string[] args)
-        {
-            //var service = 
-            //var ui = new EventConsoleUI(service);
+        using var context = new EventDBContext();
 
-            //ui.Run();
+        var eventRepository = new EventRepository(context);
+        var locationRepository = new LocationRepository(context);
+        var organizerRepository = new OrganizerRepository(context);
+        var ticketRepository = new TicketRepository(context);
+        var ticketTypeRepository = new TicketTypeRepository(context);
 
-        }
+        var eventService = new EventService(eventRepository);
+        var locationService = new LocationService(locationRepository, eventRepository);
+        var organizerService = new OrganizerService(organizerRepository);
+        var ticketTypeService = new TicketTypeService(ticketTypeRepository);
+        var ticketService = new TicketService(ticketRepository);
+
+        var ui = new EventConsoleUI(
+            eventService,
+            locationService,
+            organizerService,
+            ticketTypeService,
+            ticketService);
+
+        ui.Run();
     }
 }
